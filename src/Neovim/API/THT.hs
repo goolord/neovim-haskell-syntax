2922c814ba9e7f10b94a33922b58078e9876cb54
goolord <zacharyachurchill@gmail.com>
2019-02-28 10:52:00 -0500

    text api

---
+++ src/Neovim/API/THT.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE DeriveDataTypeable  #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE NoOverloadedStrings #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{- |
+Module      :  Neovim.API.String
+Description :  String based API
+Copyright   :  (c) Sebastian Witte
+License     :  Apache-2.0
+Maintainer  :  woozletoff@gmail.com
+Stability   :  experimental
+Note that this module is completely generated. If you're reading this on
+hackage, the actual functions of this module may be different from what is
+available to you. All the functions in this module depend on the neovim version
+that was used when this package was compiled.
+-}
+module Neovim.API.THT where
+
+import Neovim.API.TH
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Language.Haskell.TH
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import Data.Text (Text)
+
+-- | Default type mappings for the requested API.
+typeMap :: Map String (Q Type)
+typeMap = Map.fromList
+    [ ("Boolean"   , [t|Bool|])
+    , ("Integer"   , [t|Int64|])
+    , ("Float"     , [t|Double|])
+    , ("Array"     , [t|[Object]|])
+    , ("Dictionary", [t|Map Object Object|])
+    , ("String"    , [t|Text|])
+    , ("void"      , [t|()|])
+    ]
+
+-- instance NvimObject a => NvimObject (Vector a) where
+  -- toObject a = ObjectArray $ V.toList $ toObject <$> a
---
+++ src/Neovim/API/Text.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE DeriveDataTypeable  #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE NoOverloadedStrings #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{- |
+Module      :  Neovim.API.String
+Description :  String based API
+Copyright   :  (c) Sebastian Witte
+License     :  Apache-2.0
+Maintainer  :  woozletoff@gmail.com
+Stability   :  experimental
+Note that this module is completely generated. If you're reading this on
+hackage, the actual functions of this module may be different from what is
+available to you. All the functions in this module depend on the neovim version
+that was used when this package was compiled.
+-}
+module Neovim.API.Text
+    where
+
+import Neovim.API.TH
+import Neovim.API.THT
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Language.Haskell.TH
+import Data.Vector (Vector)
+import Data.Text (Text)
+
+$(generateAPI typeMap)
+
